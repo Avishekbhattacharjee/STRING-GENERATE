eb1a7ec5d67a7093930b42aa64bb3ef8f6ab3bbc
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2023-06-16 18:58:01 +0530

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
-from Stringss.Data import Data
-from Stringss.generate import ERROR_MESSAGE, generate_session
+from pyStringss.Data import Data
+from pyStringss.generate import ERROR_MESSAGE, generate_session
 from pyrogram1.types import (
     CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup)
 
