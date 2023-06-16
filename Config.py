1ba8e216ad911f081e66234c2b81a28714f2cb10
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2023-06-16 11:48:56 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-
 if os.path.exists(".env"):
     load_dotenv(".env")
 
@@ -11,3 +10,9 @@ if ENV or os.path.exists(".env"):
     from sample_config import *  # noqa
 elif os.path.exists("config.py"):
     from config import *
+
+# Define the Config class with the API_ID, API_HASH, and BOT_TOKEN attributes
+class Config:
+    API_ID = os.environ.get('API_ID')
+    API_HASH = os.environ.get('API_HASH')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
